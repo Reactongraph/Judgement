f4d197dd11a30a72e39e9abe8913bf58ea38a2c2
AbhishekOngraph <js.developer@ongrap.com>
2023-03-16 15:23:05 +0530

    changed question message

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -278,7 +278,7 @@ async function uploadMedia(media) {
     return uploadFile;
 }
 
-// use contacts send message logic
+// get user contacts & send message logic
 function getUserContacts(data, question, answerMessage) {  
   let userContacts = data.userContacts;
   if (data.isRandomize) {
@@ -288,12 +288,11 @@ function getUserContacts(data, question, answerMessage) {
   console.log("userContactssss". userContacts);
   // let contactString = encodeURIComponent("+919041823411");
   // const message = `Hey! I need your help making a quick decision. The link below will open a '${question.text}' question. Let me know what you think I should do and I'll let you know what my final decision is. \n${process.env.URL}/urls/user-response?qid=${question._id}&phid=${contactString}`;
-  //   console.log('message', message)
     // TWILIO.sendMessage(message, '+919041823411');
   for (const contact of userContacts) {
-    console.log("contact", contact)
     let contactString = encodeURIComponent(contact);
-    const message = `Hey! I need your help making a quick decision. The link below will open a '${question.text}' question. Let me know what you think I should do and I'll let you know what my final decision is. \n${process.env.URL}/urls/user-response?qid=${question._id}&phid=${contactString}`;
+    // const message = `Hey! I need your help making a quick decision. The link below will open a '${question.text}' question. Let me know what you think I should do and I'll let you know what my final decision is. \n${process.env.URL}/urls/user-response?qid=${question._id}&phid=${contactString}`;
+    const message = `Hi! This message is coming from ThisRThat. Your friend ${user.userName} needs help deciding on ${question.text}. Click on this link to send in your vote and then you'll be able to see what ${user.userName} chose. If you want help from your friends making simple decisions, download the app here. Thanks for your help!\n${process.env.URL}/urls/user-response?qid=${question._id}&phid=${contactString}`;
     TWILIO.sendMessage(message, `${contact}`);
   }
 }
